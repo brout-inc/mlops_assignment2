47dc03da927aca713a2a63b6bb6abc337942d286
brout-inc <130650143+brout-inc@users.noreply.github.com>
2024-08-05 23:20:10 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -7,11 +7,13 @@ def load_data(filepath):
     data = pd.read_csv(filepath)
     return data
 
+
 def preprocess_data(data):
     # Select features and target variable
-    X = data[['Ratings', 'Ram', 'ROM', 'Mobile_Size', 'Primary_Cam', 'Selfi_Cam', 'Battery_Power']]
+    X = data[['Ratings', 'RAM', 'ROM', 'Mobile_Size', 'Primary_Cam', 'Selfi_Cam', 'Battery_Power']]
     y = data['Price']
     return X, y
+
 
 def train_model(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -21,6 +23,7 @@ def train_model(X, y):
     mse = mean_squared_error(y_test, predictions)
     print(f'Model Mean Squared Error: {mse}')
     return model
+
 
 if __name__ == "__main__":
     data = load_data('data/mobile-price-prediction-cleaned_data.csv')
